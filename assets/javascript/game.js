f77546686e2086532e9fde02d309cdafc8331c0d
Emile Blouin <emile@D-10-19-77-101.dhcp4.washington.edu>
2019-03-08 11:48:11 -0800

    Got game to baseline functionality. Working on user feedback

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,113 @@
 class Question {
-    constructor(question, choiceOne, choiceTwo, choiceThree, choiceFour, answer) {
+    constructor(question, answers, correctAnswer) {
         this.timer = 30;
         this.question = question;
-        this.choiceOne = choiceOne;
-        this.choiceTwo = choiceTwo;
-        this.choiceThree = choiceThree;
-        this.choiceFour = choiceFour;
-        this.answer = answer;
+        this.choiceOne = answers[0];
+        this.choiceTwo = answers[1];
+        this.choiceThree = answers[2];
+        this.choiceFour = answers[3];
+        this.answer = correctAnswer;
+    }
+    renderQuestion = function (parentDiv) {
+        // Builds the current question in the div given
+        parentDiv.append(`
+        <div class="col-12">
+        <div class="jumbotron">
+        <h2 class="timer">Time Remaining: ${this.timer}</h2>
+        <div class="row">
+        <div class="col-12">
+        <h1>Question: ${this.question}</h1>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-12">
+        <button class="btn btn-primary response" onclick='game.checkAnswer("${this.choiceOne}")'>${this.choiceOne}</button>
+        <button class="btn btn-primary response" onclick='game.checkAnswer("${this.choiceTwo}")'>${this.choiceTwo}</button>
+        <button class="btn btn-primary response" onclick='game.checkAnswer("${this.choiceThree}")'>${this.choiceThree}</button>
+        <button class="btn btn-primary response" onclick='game.checkAnswer("${this.choiceFour}")'>${this.choiceFour}</button>
+        </div>
+        </div>
+        </div>
+        </div>`)
 
-        this.renderQuestion = function (parentDiv) {
-            // Builds the current question in the div given
-            parentDiv.append(`
-            <div class="col-12">
-            <div class="jumbotron">
-            <h2 class="timer">Time Remaining: ${this.timer}</h2>
-            <h1>${this.question}</h1>
-            <button class="btn btn-primary">${this.choiceOne}</button>
-            <button class="btn btn-primary">${this.choiceTwo}</button>
-            <button class="btn btn-primary">${this.choiceThree}</button>
-            <button class="btn btn-primary">${this.choiceFour}</button>
-            </div>
-            </div>`)
-
-        }
-
-        this.checkAnswer = function (answer) {
-            // Checks the inputt answer against the correct answer and reurns a boolean
-
-        }
 
     }
-
 }
 
+
 class TriviaGame {
-    constructor() {
-        this.questions = [];
+    constructor(raw) {
+        this.rawQuestions = raw;
+        this.questions;
         this.currentQuestion;
         this.correctAnswers;
         this.incorrectAnswers;
+        this.unanswered;
         this.newGame();
+        this.countdown;
     }
-
-    newGame = function () {
+    buildQuestions = () => {
+        let builtQuestions = []
+        this.rawQuestions.forEach((question) => {
+            let answers = question.incorrect_answers.concat(question.correct_answer)
+            let newQu = new Question(question.question, answers, question.correct_answer)
+            builtQuestions.push(newQu)
+        });
+        this.questions = builtQuestions
+        console.log(this.questions)
+    }
+    newGame = () => {
         // Starts a new game state. Questions need to be initialized here
-
+        this.buildQuestions(); // All of the questions need to be initialized here
+        this.pickQuestion()
     }
 
-    pickQuestion = function () {
+    pickQuestion = () => {
         // Chooses a question randomly from the question bank and removes it from the bank
-
+        if (this.questions.length > 0) {
+            this.currentQuestion = this.questions.splice((Math.floor(Math.random() * this.questions.length)), 1)[0]
+            this.currentQuestion.renderQuestion($("#main-row"))
+            this.countdown = setInterval(() => {
+                this.currentQuestion.timer--
+                $(".timer").text("Time Remaining: " + this.currentQuestion.timer)
+                if (this.currentQuestion.timer === 0) {
+                    this.checkAnswer("timeout")
+                }
+            }, 1000)
+        } else {
+            alert("ask new game")
+        }
     }
 
-    updateDisplay = function () {
-        // Updates all the existing displays
+    checkAnswer = (choice) => {
+        clearInterval(this.countdown)
+        $("#main-row").empty()
+        if (choice === this.currentQuestion.answer) {
+            this.correctAnswers++;
+        } else if (choice === "timeout") {
+            this.unanswered++;
+        } else {
+            this.incorrectAnswers++;
+        }
 
+        this.countdown = setTimeout(() => {
+            $("#main-row").empty()
+            this.pickQuestion()
+        }, 5000)
     }
 
-    chooseAnswer = function () {
-        // Reacts to the user clicking any of the buttons
 
-    }
 }
+let game;
 
+function start() {
+    $.ajax({
+        url: "https://opentdb.com/api.php?amount=20&category=20&type=multiple",
+        complete: (result) => {
+            game = new TriviaGame(result.responseJSON.results)
+        }
+    })
+}
 
 // User goes to the website. They see an entrance screen with a push button to start
 
@@ -85,6 +130,3 @@ class TriviaGame {
 // After thirty seconds, a screen pops up saying they ran out of time displaying the correct answer
 
 // After five seconds, the game ends and a screen displays showing the number of correct answers, incorrect answers, and a newgame screen.
-
-let question = new Question("test", "a", "b", "c", "d", "c")
-question.renderQuestion($("#main-row"))
